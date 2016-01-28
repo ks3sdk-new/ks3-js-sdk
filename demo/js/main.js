078cbdf4a396915b26a879bd7b41a06e224cf5c6
Chen Jin <chenjin@kingsoft.com>
2016-01-28 21:39:31 +0800

    add demo for uploading file via independent put request (backend service Not Required)

--- demo/js/main.js
+++ demo/js/main.js
@@ -5,16 +5,18 @@
      */
     var Constants = {
         AK: "S1guCl0KF/pEoT6TKTR1",   //replace with your AK
-        UPLOAD_HTTP_URL: "http://kssws.ks-cdn.com/",
-        UPLOAD_HTTPS_URL: "https://kss.ksyun.com/"    //杭州region
+        UPLOAD_HTTP_URL: "http://kss.ksyun.com/",
+        UPLOAD_HTTPS_URL: "https://kss.ksyun.com/"    //杭州region 其他region的域名参见：http://ks3.ksyun.com/doc/api/index.html
     };
+
+    var SK = 'your secret key'; //注意：不安全，如果前端计算signature，请确保不会泄露SK
 
     var filelistNode = document.getElementById('filelist')
     var bucketName = "bucket4jssdk";
 
-    //如果bucket不是公开读写的，需要先鉴权，即提供policy和signature表单域
+    //如果bucket不是公开读写的，需要先鉴权，即提供policy和signature表单
     var policy = {
-        "expiration": "2016-02-01T12:00:00.000Z",
+        "expiration": "2016-02-02T12:00:00.000Z",
         "conditions": [
             ["eq","$bucket", bucketName],
             ["starts-with", "$key", ""],
@@ -25,8 +27,9 @@
     //policy stringify再经过BASE64加密后的字符串（utf8编码格式）
     var stringToSign = Ks3.Base64.encode(JSON.stringify(policy));
 
-    //从后端sdk获取signature签名  算法为：Signature = Base64(HMAC-SHA1(YourSecretKey, - stringToSign ) );
-    var signature = 'ZpsVUIVcwd91JM8uCwvqWUrnI54=';
+    //建议从后端sdk获取signature签名  算法为：Signature = Base64(HMAC-SHA1(YourSecretKey, stringToSign ) );
+    var signatureFromPolicy = b64_hmac_sha1(SK, stringToSign);
+    console.log(signatureFromPolicy);
 
 
     var ks3UploadUrl;
@@ -40,7 +43,7 @@
     var ks3Options = {
         KSSAccessKeyId: Constants['AK'],
         policy: stringToSign,
-        signature: signature,
+        signature: signatureFromPolicy,
         bucket_name: bucketName,
         key: '${filename}',
         acl: "public-read",
@@ -59,11 +62,54 @@
             var linkNode = itemNode.querySelector('a');
             linkNode.href = ks3Options.uploadDomain + "/" + obj.name;
             linkNode.innerHTML = obj.name;
+
+            //增加加水印按钮
+            var adpBtn = document.createElement("button");
+            adpBtn.innerHTML = '添加水印';
+            adpBtn.onclick = function(){
+                var url = ks3UploadUrl + bucketName + '/' + obj.name + '?adp';
+                var kssHeaders = {
+                    'kss-async-process': 'tag=imgWaterMark&type=2&dissolve=65&gravity=NorthEast&text=6YeR5bGx5LqR&font=5b6u6L2v6ZuF6buR&fill=I2JmMTcxNw==&fontsize=500&dy=10&dx=20|tag=saveas&bucket=' + bucketName + '&object=imgWaterMark-' + obj.name,
+                    'kss-notifyurl': 'http://10.4.2.38:19090/'
+                };
+                var signature = generateToken(SK, bucketName, obj.name, 'PUT', kssHeaders);
+                var xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4) {
+                        if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
+                            alert("put请求成功");
+                            var waterMarkImgLink = document.createElement('a');
+                            waterMarkImgLink.setAttribute('target','_blank');
+                            waterMarkImgLink.style.marginLeft = "30px";
+                            waterMarkImgLink.innerHTML = "imgWaterMark-" + obj.name;
+
+                            //一分钟后的时间戳
+                            var  timeStampIn60Second = new Date((new Date()).valueOf() + 1000*60).valueOf();
+                            setTimeout(function(){ //异步任务，等1秒再看处理结果
+                                waterMarkImgLink.href = ks3UploadUrl + bucketName + '/imgWaterMark-' + obj.name + '?KSSAccessKeyId=' +  encodeURIComponent(Constants['AK']) + '&Expires=' + timeStampIn60Second + '&Signature=' + encodeURIComponent(signature);
+                                itemNode.appendChild(waterMarkImgLink);
+                            },1000);
+
+                        }else{
+                            alert('Request was unsuccessful: ' + xhr.status);
+                        }
+                    }
+                };
+
+                xhr.open("put", url, true);
+
+                xhr.setRequestHeader('Authorization','KSS ' + Constants['AK'] + ':' + signature );
+                xhr.setRequestHeader('kss-async-process', kssHeaders['kss-async-process']);
+                xhr.setRequestHeader('kss-notifyurl',kssHeaders['kss-notifyurl']); //替换成您接收异步处理任务完成通知的url地址
+                xhr.send(null);
+            };
+            itemNode.appendChild(adpBtn);
+
         },
         onFilesAddedCallBack: function(uploader, objArray){ // objArray是等待上传的文件对象的数组
             for (var i = 0 ; i < objArray.length ; i++){
                 var itemNode = document.createElement("li");
-                itemNode.innerHTML = objArray[i].name + "<span style='margin:5px 20px;'></span><a></a>";
+                itemNode.innerHTML = objArray[i].name + "<span style='margin:5px 20px;'></span><a style='margin-right: 20px;' target='_blank'></a>";
                 itemNode.id = objArray[i].id;
                 filelistNode.appendChild(itemNode);
             }
@@ -126,6 +172,7 @@
      *  注： 这里使用了FormData序列化表单中选取的文件，XMLHttpRequest 2级定义了FormData类型，
      *  支持的浏览器有Firefox 4+，Safari 5+，Chrome 和 Android 3+版的WebKit
      */
+
     document.getElementById('utp').onclick = function() {
         var imgFile = document.getElementById('imgFile').files[0]; //获取文件对象
         var formData = new FormData();
@@ -134,6 +181,11 @@
         formData.append("file", imgFile);
         var bucketName = "bucket4jssdk";
         var url = 'http://127.0.0.1:3000/' + bucketName ;
+        var kssHeaders = {
+            'kss-async-process': 'tag=imgWaterMark&type=2&dissolve=65&gravity=NorthEast&text=6YeR5bGx5LqR&font=5b6u6L2v6ZuF6buR&fill=I2JmMTcxNw==&fontsize=500&dy=10&dx=20|tag=saveas&bucket=' + bucketName + '&object=imgWaterMark-' + objKey,
+            'kss-notifyurl': 'http://10.4.2.38:19090/'
+        };
+
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (xhr.readyState == 4) {
@@ -163,9 +215,68 @@
         //xhr.setRequestHeader('Content-Length',imgFile.size);
 
         xhr.setRequestHeader('Authorization','KSS ' + Constants['AK'] );
-        xhr.setRequestHeader('kss-async-process','tag=imgWaterMark&type=2&dissolve=65&gravity=NorthEast&text=6YeR5bGx5LqR&font=5b6u6L2v6ZuF6buR&fill=I2JmMTcxNw==&fontsize=500&dy=10&dx=20|tag=saveas&bucket=' + bucketName + '&object=imgWaterMark-' + objKey );
-        xhr.setRequestHeader('kss-notifyurl','http://10.4.2.38:19090/'); //替换成您接收异步处理任务完成通知的url地址
+        xhr.setRequestHeader('kss-async-process', kssHeaders['kss-async-process']);
+        xhr.setRequestHeader('kss-notifyurl',kssHeaders['kss-notifyurl']); //替换成您接收异步处理任务完成通知的url地址
         xhr.send(formData);
     };
+
+    /**
+     *  PUT Object 上传触发处理示例2（不依赖与后端）
+     *  前端计算signature，put请求直接到ks3 API
+     *  注意：容易泄露SK, 建议只用于内部项目
+     *
+     */
+
+    document.getElementById('utp2').onclick = function() {
+        var imgFile = document.getElementById('imgFile2').files[0]; //获取文件对象
+        var formData = new FormData();
+        var objKey = imgFile.name;
+        formData.append("file", imgFile);
+        var bucketName = "bucket4jssdk";
+        var url = 'http://'+ bucketName + '.kss.ksyun.com/' + objKey;
+        var kssHeaders = {
+            'kss-async-process': 'tag=imgWaterMark&type=2&dissolve=65&gravity=NorthEast&text=6YeR5bGx5LqR&font=5b6u6L2v6ZuF6buR&fill=I2JmMTcxNw==&fontsize=500&dy=10&dx=20|tag=saveas&bucket=' + bucketName + '&object=imgWaterMark-' + objKey,
+            'kss-notifyurl': 'http://10.4.2.38:19090/'
+        };
+        var signature = '';
+
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){
+                    alert("上传触发处理成功");
+                    var waterMarkImg = document.getElementById('display-adp-result2').firstChild;
+                    //一分钟后的时间戳
+                    var  timeStampIn60Second = new Date((new Date()).valueOf() + 1000*60).valueOf();
+                    setTimeout(function(){
+                        //异步任务，等两秒
+                        waterMarkImg.src = 'http://kss.ksyun.com/' + bucketName + '/imgWaterMark-' + objKey + '?KSSAccessKeyId=' +  encodeURIComponent(Constants['AK']) + '&Expires=' + timeStampIn60Second + '&Signature=' + encodeURIComponent(signature);
+                    },2000);
+
+                }else{
+                    alert('Request was unsuccessful: ' + xhr.status);
+                }
+            }
+        };
+
+        function progressFunction(e) {
+            var progressBar = document.getElementById("progressBar2");
+            if (e.lengthComputable) {
+                progressBar.max = e.total;
+                progressBar.value = e.loaded;
+            }
+        }
+        xhr.upload.addEventListener("progress", progressFunction, false);
+        xhr.open("put", url, true);
+
+        signature = generateToken(SK, bucketName, objKey, 'PUT', kssHeaders);
+
+        xhr.setRequestHeader('Authorization','KSS ' + Constants['AK'] + ':' + signature );
+        xhr.setRequestHeader('kss-async-process', kssHeaders['kss-async-process']);
+        xhr.setRequestHeader('kss-notifyurl',kssHeaders['kss-notifyurl']); //替换成您接收异步处理任务完成通知的url地址
+        xhr.send(formData);
+    };
+
+
 
 })();
